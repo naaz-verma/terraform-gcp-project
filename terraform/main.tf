6d8bdc03f25510168c0e70b1ecc3149d806b3f87
ASUS <naazv05@gmail.com>
2025-07-19 21:04:39 +0530

    main.tf fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,28 +4,12 @@ terraform {
     prefix = "jenkins/dev"
   }
 }
+
 provider "google" {
   credentials = file("credentials.json")
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-}
-
-resource "google_compute_instance" "my_vm" {
-  name         = var.vm_name
-  machine_type = "f1-micro"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
 }
 
 resource "random_id" "bucket_suffix" {
